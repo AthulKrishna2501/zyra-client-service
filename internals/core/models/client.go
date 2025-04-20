af4de9768547d7aea209ef0460cd23cc613aa893
Athul Krishna <akkv.2507@gmail.com>
2025-04-20 18:39:42 +0530

    refactor(transaction model): added paymentIntentId field

--- internals/core/models/client.go
+++ internals/core/models/client.go
@@ -8,15 +8,16 @@ import (
 )
 
 type Transaction struct {
-	ID            uuid.UUID      `json:"id" gorm:"type:uuid;primary_key"`
-	TransactionID uuid.UUID      `json:"transaction_id" gorm:"type:uuid;unique;not null"`
-	UserID        uuid.UUID      `json:"user_id" gorm:"type:uuid;not null;index"`
-	Purpose       string         `json:"purpose" gorm:"not null"`
-	AmountPaid    int            `json:"amount_paid" gorm:"not null"`
-	PaymentMethod string         `json:"payment_method" gorm:"type:varchar(50);not null"`
-	PaymentStatus string         `json:"payment_status" gorm:"type:varchar(50);not null;index"`
-	DateOfPayment time.Time      `json:"date_of_payment" gorm:"not null;index"`
-	User          authModel.User `json:"user" gorm:"foreignKey:UserID"`
+	ID              uuid.UUID      `json:"id" gorm:"type:uuid;primary_key"`
+	TransactionID   uuid.UUID      `json:"transaction_id" gorm:"type:uuid;unique;not null"`
+	UserID          uuid.UUID      `json:"user_id" gorm:"type:uuid;not null;index"`
+	PaymentIntentID string         `json:"payment_intent_id" gorm:"type:varchar(255);index"`
+	Purpose         string         `json:"purpose" gorm:"not null"`
+	AmountPaid      int            `json:"amount_paid" gorm:"not null"`
+	PaymentMethod   string         `json:"payment_method" gorm:"type:varchar(50);not null"`
+	PaymentStatus   string         `json:"payment_status" gorm:"type:varchar(50);not null;index"`
+	DateOfPayment   time.Time      `json:"date_of_payment" gorm:"not null;index"`
+	User            authModel.User `json:"user" gorm:"foreignKey:UserID"`
 }
 
 type Event struct {
